71f5cfbe7b33250836e17c1f44d32a46814a98ad
brett <brettinternet@gmail.com>
2021-12-11 13:13:21 -0700

    build: cross-platform cat to prevent console clearing in cra

---
+++ scripts/start-cra.ts
@@ -0,0 +1,17 @@
+import { spawn } from 'child_process'
+import { resolve } from 'path'
+
+const cwd = resolve(__dirname, '..')
+
+/**
+ * Because CRA refuses to allow users from preventing console clearing
+ * https://github.com/facebook/create-react-app/issues/2495
+ *
+ * This is the cross-platform equivalent to piping to `cat`
+ */
+const start = spawn('npx', ['craco', 'start'], {
+  shell: true,
+  cwd,
+})
+
+start.stdout.pipe(process.stdout)
