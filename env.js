87c07b5fcb6446c85ff957508264186acae42704
brett <brettinternet@gmail.com>
2023-04-01 01:46:25 -0600

    add version to env

--- env.js
+++ env.js
@@ -1,11 +1,14 @@
 /**
  * Env vars provided to craco and esbuild configs
  */
+const IS_PROD = process.env.NODE_ENV === 'production'
+const BUILD_VERSION = IS_PROD ? '0.0.1 alpha' : 'dev'
+
 module.exports = {
-  IS_PROD: process.env.NODE_ENV === 'production',
+  IS_PROD,
   IS_CHROME: process.env.TARGET === 'chrome',
   IS_FIREFOX: process.env.TARGET === 'firefox',
-  BUILD_VERSION: process.env.BUILD_VERSION || 'dev',
+  BUILD_VERSION,
   BUILD_TIME: process.env.BUILD_TIME || new Date().toISOString(),
   // feature flags
   FEATURE_SAVE_SESSIONS: process.env.FEATURE_SAVE_SESSIONS === 'true',
